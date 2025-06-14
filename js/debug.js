efc42a1138c445331396f70f9dfb3a5afacd533b
toocheesy <thadduscard@gmail.com>
2025-06-14 11:36:00 -0400

    � STACKED! Updated debug

--- js/debug.js
+++ js/debug.js
@@ -1,5 +1,5 @@
 // debug.js
-export function debugLog(...args) {
+export function logDebug(...args) {
   if (typeof window !== 'undefined' && window.state?.settings?.debugMode) {
     console.log('[DEBUG]', ...args);
   }
